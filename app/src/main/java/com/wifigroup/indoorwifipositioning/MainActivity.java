99ee9522fc067f37e198f1e7d4170977c7e18f85
GabrieleTriglia <gabrieletriglia04@gmail.com>
2026-05-27 10:13:11 +0200

    training_fragments.xml UI: modifications

--- app/src/main/java/com/wifigroup/indoorwifipositioning/MainActivity.java
+++ app/src/main/java/com/wifigroup/indoorwifipositioning/MainActivity.java
@@ -1,10 +1,16 @@
 package com.wifigroup.indoorwifipositioning;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+    private Button bttTraining;
+    private Button bttDemo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
